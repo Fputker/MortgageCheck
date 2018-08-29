75c83a36209c1919a538789765d442133bfa63d0
Putker <florian.putker@capgemini.com>
2018-08-29 11:06:14 +0100

    First commit

---
+++ src/main/java/mortgage/Calculation.java
@@ -0,0 +1,13 @@
+package mortgage;
+
+public class Calculation {
+
+    private float income;
+    private float loanValue;
+    private float homeValue;
+    private float monthlyCosts;
+    private boolean mortgageApproved;
+
+
+
+}
---
+++ src/main/java/mortgage/CalculationController.java
@@ -0,0 +1,4 @@
+package mortgage;
+
+public class CalculationController {
+}
